a8d3874a89bea2b6d59fbce56514cd18629b543e
Julio Armando Gonzalez Garcia <GGJ.YABOOK@gmail.com>
2025-11-24 20:28:48 +0000

    El nombre del doctor es Dr. Fernando Abraham Muñoz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -195,7 +195,7 @@ export default function Home() {
                  <div className="flex flex-col sm:flex-row items-center gap-8">
                      <Image src="/images/c2.png" width={200} height={300} className="rounded-lg shadow-md object-cover" data-ai-hint="doctor portrait"/>
                     <div>
-                        <h3 className="font-headline text-2xl font-semibold text-primary">Dr. Carlos López</h3>
+                        <h3 className="font-headline text-2xl font-semibold text-primary">Dr. Fernando Abraham Muñoz</h3>
                         <p className="mt-4 text-muted-foreground">Urólogo formado en Medicina por la Universidad La Salle y especializado en Urología por la UNAM. Cuenta con experiencia en cirugía laparoscópica y mínima invasión, endourología, urología oncológica y tratamiento de disfunción eréctil.</p>
                         <p className="mt-2 text-muted-foreground">Ha realizado entrenamiento nacional e internacional en cirugía láser de próstata, lo que respalda su práctica con técnicas modernas y seguras.</p>
                         <p className="mt-2 text-muted-foreground">Ofrece atención basada en honestidad, ética y calidez humana. Es miembro de la SMU, AUA, EAU y CAU.</p>
