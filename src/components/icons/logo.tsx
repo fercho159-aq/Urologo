6743e2872b2981a7d68131e9a0f2b685e18612f4
MAW SOLUCIONES <solucionesmaw@gmail.com>
2025-11-25 00:44:00 +0000

    sigue sobresaliendo colocalo a 70px de altura

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export default function Logo({ className, ...props }: { className?: string }) {
   return (
-    <div className={cn("relative h-12 w-auto", className)} {...props}>
+    <div className={cn("relative h-auto w-auto", className)} {...props}>
       <Image 
         src="/images/logo.png" 
         alt="Urolog Logo" 
         width={150}
-        height={50}
+        height={70}
         className="object-contain"
       />
     </div>
